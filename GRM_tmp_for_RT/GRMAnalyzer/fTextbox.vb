547acf767abb7c0c3a58ca81da1a7d2d1d02bfbf
Yun Seok Choi <yschoi51@kict.re.kr>
2019-11-28 10:22:52 +0900

    update manuals

--- GRM_tmp_for_RT/GRMAnalyzer/fTextbox.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class fTextBox
-
-    Private Sub txtTextBox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtTextBox.KeyPress
-        If e.KeyChar = Convert.ToChar(1) Then
-            DirectCast(sender, TextBox).SelectAll()
-            e.Handled = True
-        End If
-    End Sub
-End Class
--- GRM_tmp_for_RT/README.md
+++
@@ -1,4 +0,0 @@
-# GRM(Grid based Rainfall-Runoff Model) 
-  - Temporary project for updating RealTime module of stable version 
-  - This project will be combined with latest verion in eary, 2020.
-   
